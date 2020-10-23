12536226fb923d04195d0c2019598b5cb303d004
bartosz Minczuk <bminczu@gmail.com>
2020-10-23 16:20:38 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,10 +1,11 @@
 require_relative './part_1_solution.rb'
 require 'pry'
 def apply_coupons(cart, coupons)
-  cart.each do cart[key]
-  binding.pry# Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i =0 # Consult README for inputs and outputs
+coupons.each do |key|
+  discount_item = find_item_by_name_in_collection[:item], cart)
+  filled_cart = !!discount_item
+  cart.each do |key|# REMEMBER: This method **should** update cart
 end
 
 def apply_clearance(cart)
