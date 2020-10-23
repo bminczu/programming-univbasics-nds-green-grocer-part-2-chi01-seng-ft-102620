fe18f33e81e2b02aa874f9d9c86eda999924f232
bartosz Minczuk <bminczu@gmail.com>
2020-10-23 16:34:31 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -3,9 +3,18 @@ require 'pry'
 def apply_coupons(cart, coupons)
   i =0 # Consult README for inputs and outputs
 coupons.each do |key|
-  discount_item = find_item_by_name_in_collection[:item], cart)
-  filled_cart = !!discount_item
-  cart.each do |key|# REMEMBER: This method **should** update cart
+  discount_item = find_item_by_name_in_collection([:item], cart)
+  in_cart = !!discount_item
+  valid_coupon = in_cart && coupons[:num]<= discount_item[:count]
+  if discount_item && valid_coupon
+    cart << {item: "#{coupons[:item]} W/ COUPON",
+    price: coupon[:cost]/coupon[:num],
+    clearance: discount_item[:clearance],
+    count: coupon[:num]
+  }# REMEMBER: This method **should** update cart
+  end
+end
+cart
 end
 
 def apply_clearance(cart)
